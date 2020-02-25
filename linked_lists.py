cd4383678bba80293b796a1fbcc21de7dfe3e286
lizzab <Jjibber12>
2020-02-24 19:25:05 -0500

    Commit

--- linked_lists.py
+++ linked_lists.py
@@ -27,3 +27,32 @@ class SingleLinkedList:
 
     def prepend(self, data):
         self.head = ListNode(data=data, next=self.head)
+
+    def add_head(self, data):
+        new_node = ListNode(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def add_tail(self, data):
+        new_node = ListNode(data)
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = new_node
+
+    def remove_head(self):
+        new_node = self.head
+        self.head = self.head.next
+        return new_node.data
+
+    def remove_tail(self):
+        curr_node = self.head
+        previous_node = self.head
+        while curr_node.next:
+            previous_node = curr_node
+            curr_node = curr_node.next
+        previous_node.next = None
+        return curr_node.data
+
+    def clear_list(self):
+        self.head = None
